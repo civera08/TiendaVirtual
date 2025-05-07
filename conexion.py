b6a6a68dbc5c708711030b0f92a2cd6810b5205f
César Rivera <cesar_riv15@outlook.com>
2025-05-07 00:20:18 -0600

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
 
+# Conexión con credenciales de Windows
 def obtener_engine():
-    server = 'NEURABYTE'
-    database = 'TiendaVirtual'
+    server = 'TuServidor'
+    database = 'TuDB'
     connection_string = (
         f"mssql+pyodbc://@{server}/{database}"
         "?driver=ODBC+Driver+17+for+SQL+Server"
         "&trusted_connection=yes"
     )
+# Para autenticación SQL Server (usuario y contraseña)
+#connection_string = (
+#   f"mssql+pyodbc://{usuario}:{contraseña}@{server}/{database}"
+#    "?driver=ODBC+Driver+17+for+SQL+Server"
+    
     return create_engine(connection_string)
