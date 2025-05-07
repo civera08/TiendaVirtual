991364557916478fd8fd4b48e70cdc98f2241929
César Rivera <cesar_riv15@outlook.com>
2025-05-07 17:42:57 -0600

    Update regresion.py

--- regresion.py
+++ regresion.py
@@ -1,4 +1,4 @@
-# Copyright (c) [Año] [Tu Nombre]
+# Copyright (c) 2025 César Rivera
 # Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 import pandas as pd
